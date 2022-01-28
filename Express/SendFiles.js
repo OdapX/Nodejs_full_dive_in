3c98b5c8b536a7b15a2aa5da622a4987be3e731b
yahya benzha <yahya_benzha@um5.ac.ma>
2022-01-28 22:49:04 +0100

    SEnding Files with node

---
+++ Express/SendFiles.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const mainRouter = express.Router();
+
+mainRouter.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, ".", "Images", "libuv.png"));
+});
+
+app.use("/Image", mainRouter);
+
+app.listen(3000, () => {
+  console.log("listening..");
+});
