d81022242cd15c91bcaa08fbfc244cd1336f6fd3
yahya benzha <yahya_benzha@um5.ac.ma>
2022-01-28 23:15:23 +0100

    serving a static site with express

--- Express/SendFiles.js
+++ Express/SendFiles.js
@@ -6,7 +6,7 @@ const app = express();
 const mainRouter = express.Router();
 
 mainRouter.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, ".", "Images", "libuv.png"));
+  res.sendFile(path.join(__dirname, "Images", "libuv.png"));
 });
 
 app.use("/Image", mainRouter);
