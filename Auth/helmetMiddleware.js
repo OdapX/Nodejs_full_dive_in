bfd5fdc04a29c6645cbccff0919799c1529eaf05
yahya benzha <yahya_benzha@um5.ac.ma>
2022-02-20 19:01:21 +0100

    cookie based sessions

--- Auth/helmetMiddleware.js
+++ Auth/helmetMiddleware.js
@@ -1,32 +1,97 @@
 const fs = require("fs");
-const https = require("https");
-const express = require("express");
-const helmet = require("helmet");
-const app = express();
-
 require("dotenv").config();
-
-app.use(helmet());
+const path = require("path");
+const https = require("https");
+const helmet = require("helmet");
+const express = require("express");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const { Strategy } = require("passport-google-oauth20");
 
 const configs = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.Cookie_key_1,
+  COOKIE_KEY_2: process.env.Cookie_key_2,
 };
-const Auth_options = {
-  CALLBACK_URL: "/auth/google/callback",
-  CLIENT_ID: configs.CLIENT_ID,
-  CLIENT_SECRET: configs.CLIENT_SECRET,
-};
-// passport.use(new Strategy());
 
-app.get("/sensitive", (req, res) => {
+const Auth_options = {
+  callbackURL: "/auth/google/callback",
+  clientID: configs.CLIENT_ID,
+  clientSecret: configs.CLIENT_SECRET,
+};
+
+passport.serializeUser((user, done) => {
+  console.log(user);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  done(null, id);
+});
+const app = express();
+app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000, //1 day
+    keys: [configs.COOKIE_KEY_1, configs.COOKIE_KEY_2],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  done(null, profile);
+}
+passport.use(new Strategy(Auth_options, verifyCallback));
+
+function LoggedIn(req, res, next) {
+  const isLoggedIn = req.isAuthenticated() && req.user;
+  if (!isLoggedIn) {
+    return res.status(400).json({ message: "please Log in first" });
+  }
+  next();
+}
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+    session: true,
+  }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
+app.get("/auth/logout", (req, res) => {
+  req.logout();
+  return res.redirect("/");
+});
+
+app.get("/sensitive", LoggedIn, (req, res) => {
   res.send("sensitive data");
+});
+
+app.get("/", (req, res) => {
+  return res.status(200).send("GOT /");
 });
 
 const server = https.createServer(
   {
-    key: fs.readFileSync("key.pem"),
-    cert: fs.readFileSync("cert.pem"),
+    key: fs.readFileSync(path.join(__dirname, "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert.pem")),
   },
   app
 );
@@ -36,6 +101,6 @@ server.listen(3000, () => {
 });
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log("google profile", profile);
+  //console.log("google profile", profile);
   done(null, profile);
 }
