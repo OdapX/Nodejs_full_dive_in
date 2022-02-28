715d20470a0bb52998bb7e2619d332346266b180
yahya benzha <yahya_benzha@um5.ac.ma>
2022-02-28 23:44:20 +0100

    migrated to apollo server

--- graphQL/server.js
+++ graphQL/server.js
@@ -1,27 +1,30 @@
 const path = require("path");
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+
+const { ApolloServer } = require("apollo-server-express");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 
 const TypeArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 const ResolverArray = loadFilesSync(path.join(__dirname, "**/*.resolver.js"));
 
-const Schema = makeExecutableSchema({
-  typeDefs: TypeArray,
-  resolvers: ResolverArray,
-});
+async function StartApolloServer() {
+  app = express();
+  const schema = makeExecutableSchema({
+    typeDefs: TypeArray,
+    resolvers: ResolverArray,
+  });
 
-const app = express();
+  const server = new ApolloServer({
+    schema,
+  });
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: Schema,
-    graphiql: true,
-  })
-);
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
 
-app.listen(3000, () => {
-  console.log("listening on port 3000..");
-});
+  app.listen(3000, () => {
+    console.log("listening on port 3000..");
+  });
+}
+
+StartApolloServer();
