ca6d06afb453b2c3495d126eb234105c791a61aa
yahya benzha <yahya_benzha@um5.ac.ma>
2022-02-28 15:48:43 +0100

    ecom schema

--- graphQL/server.js
+++ graphQL/server.js
@@ -3,13 +3,58 @@ const { buildSchema } = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
 const Schema = buildSchema(`
   type Query{
-      description:String
-      price: Float
+      Products : [Product]   
+      Orders : [Order]
   }
+
+  type Product {
+     id :ID!
+     description:String
+     price : Float
+     reviews : [Review]
+  }
+  type Review {
+    rating : Int!
+    comment : String
+  }
+
+  type Order {
+    id : ID!
+    date : String
+    items : [Item]
+  }
+  type Item {
+      product : Product!
+      quantity : Int!
+  }
+  
+
 `);
 const root = {
-  description: "jacket",
-  price: 250,
+  Products: [
+    {
+      id: "Product1",
+      description: "cool product",
+      price: 99.99,
+      reviews: [{ rating: 5, comment: "excellent" }],
+    },
+  ],
+  Orders: [
+    {
+      id: "Order1",
+      date: "02-02-2022",
+      items: [
+        {
+          product: {
+            id: "Product1",
+            description: "old cool product",
+            Price: 79.99,
+          },
+          quantity: 3,
+        },
+      ],
+    },
+  ],
 };
 const app = express();
 
