7b945daab8a20ee027b52f2bfcddcc8e18baa401
yahya benzha <yahya_benzha@um5.ac.ma>
2022-02-28 22:19:07 +0100

    filtering and mutations

--- graphQL/Products/products.model.js
+++ graphQL/Products/products.model.js
@@ -5,9 +5,59 @@ const products = [
     price: 99.99,
     reviews: [{ rating: 5, comment: "excellent" }],
   },
+  {
+    id: "Product2",
+    description: "cool product2",
+    price: 25,
+    reviews: [{ rating: 5, comment: "excellent" }],
+  },
 ];
 
 function GetProducts() {
   return products;
 }
-module.exports = GetProducts;
+
+function GetProductsByPrice(min, max) {
+  return products.filter((product) => {
+    return product.price <= max && min <= product.price;
+  });
+}
+function GetProductByID(id) {
+  return products.find((product) => {
+    return product.id === id;
+  });
+}
+
+function AddNewProduct(id, description, price) {
+  const NewProduct = {
+    id: id,
+    description: description,
+    price: price,
+    reviews: [],
+  };
+  const Old_instance = products.find((product) => {
+    return product.id === id;
+  });
+  if (!Old_instance) {
+    products.push(NewProduct);
+    console.log("Added !");
+    return NewProduct;
+  }
+  return { id: "already exists" };
+}
+
+function AddNewReviews(id, rating, comment) {
+  products
+    .find((product) => product.id === id)
+    .reviews.push({ rating: rating, comment: comment });
+
+  return products.find((product) => product.id === id);
+}
+
+module.exports = {
+  GetProducts,
+  GetProductsByPrice,
+  GetProductByID,
+  AddNewProduct,
+  AddNewReviews,
+};
--- graphQL/Products/products.resolver.js
+++ graphQL/Products/products.resolver.js
@@ -1,8 +1,28 @@
-const GetProducts = require("./products.model");
+const {
+  GetProducts,
+  GetProductsByPrice,
+  GetProductByID,
+  AddNewProduct,
+  AddNewReviews,
+} = require("./products.model");
 module.exports = {
   Query: {
     Products: () => {
       return GetProducts();
+    },
+    ProductsByPrice: (_, args) => {
+      return GetProductsByPrice(args.min, args.max);
+    },
+    ProductById: (_, args) => {
+      return GetProductByID(args.id);
+    },
+  },
+  Mutation: {
+    AddNewProduct: (_, args) => {
+      return AddNewProduct(args.id, args.description, args.price);
+    },
+    AddNewReview: (_, args) => {
+      return AddNewReviews(args.id, args.rating, args.comment);
     },
   },
 };
